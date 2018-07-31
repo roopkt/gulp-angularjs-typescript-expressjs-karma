e34f2e638cf128a494301bb4d353d679f96c2e34
roopkt <roopkt@gmail.com>
2018-07-31 13:28:58 -0400

    cleaned up not need files

--- src/_references.ts
+++
@@ -1,2 +0,0 @@
-// ///<reference path="../lib/typedefinitions/angular/angular.d.ts"/>
-// ///<reference path="../lib/typedefinitions/angular/angular-route.d.ts"/>
--- src/client/_references.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// ///<reference path="../lib/typedefinitions/angular/angular.d.ts"/>
-// ///<reference path="../lib/typedefinitions/angular/angular-route.d.ts"/>
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNsaWVudC9fcmVmZXJlbmNlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEscUVBQXFFO0FBQ3JFLDJFQUEyRSIsImZpbGUiOiJjbGllbnQvX3JlZmVyZW5jZXMuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyAvLy88cmVmZXJlbmNlIHBhdGg9XCIuLi9saWIvdHlwZWRlZmluaXRpb25zL2FuZ3VsYXIvYW5ndWxhci5kLnRzXCIvPlxuLy8gLy8vPHJlZmVyZW5jZSBwYXRoPVwiLi4vbGliL3R5cGVkZWZpbml0aW9ucy9hbmd1bGFyL2FuZ3VsYXItcm91dGUuZC50c1wiLz5cbiJdfQ==
